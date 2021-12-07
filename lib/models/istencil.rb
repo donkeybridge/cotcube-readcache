3ecc3b5bd6d0f9eca08919aad3a06c1709693480
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-12-07 20:24:24 +0000

    models/istencil: applied private attr_reader
       implementend next_eod / next_eow

--- lib/models/istencil.rb
+++ lib/models/istencil.rb
@@ -20,25 +20,25 @@ module Cotcube
           readcache,
           asset: :full,
           interval: 30.minutes,
-          timezone: Cotcube::Helpers::CHICAGO,
-          datetime: Cotcube::Helpers::CHICAGO.now  # meant for testing purposes, dont use in production
+          timezone: Cotcube::Helpers::CHICAGO
         )
-          @datetime = datetime
           @interval = interval
           @timezone = timezone
           @asset    = asset
-          @istencil = Cotcube::Level::Intraday_Stencil.new(interval: @interval, swap_type: :full, asset: @asset, datetime: @datetime)
+          @readcache= readcache
+          update
         end
 
         def update
-          @datetime = @timezone.now
+          @datetime = timezone.now
           @next_eod = nil
+          @next_eow = nil
           @until    = nil 
-          @istencil = Cotcube::Level::Intraday_Stencil.new(interval: @interval, swap_type: :full, asset: @asset, datetime: @datetime)
+          @istencil = Cotcube::Level::Intraday_Stencil.new(interval: interval, swap_type: :full, asset: asset, datetime: datetime)
         end
 
         def payload
-          @istencil.base.select{|z| z[:x] > -6 }
+          istencil.base.select{|z| z[:x] > -6 }
         end
 
         # given now its  9:12, the current stencil.index would be at  8:30, its valid from  9:00 to  9:30, and it would switch to  9:00 at 9:30
@@ -47,16 +47,16 @@ module Cotcube
         # the current stencil is valid until 30 minutes after the beginning  of next stencil
 
         def valid_until
-          @until ||= @istencil.index(1)[:datetime] + @interval
+          @until ||= istencil.index(1)[:datetime] + interval
         end
 
         alias_method :next_eop, :valid_until
 
         def expired?
-          valid_until < @timezone.now
+          valid_until < timezone.now
         end
 
-        def modified_at;                   @datetime; end
+        def modified_at; datetime; end
 
         # the start of EOD is currently defined as 16:00 CT of the current business day (CBD). 
         #   What happens after 16:00 (or more precisely 17:00, as there is 1 hour of general
@@ -65,20 +65,35 @@ module Cotcube
         # if there is a dayswitch between i and i + 1, then we have found the EOD at i
         # likewise, if there is a gap around 16 between i and i + 1
         def next_eod
-          unless @next_eod
+          if @next_eod.nil?
             i = 1
             loop do 
-              cur = @istencil.index(i  )[:datetime] + @interval
-              nxt = @istencil.index(i+1)[:datetime] + @interval
-              break if (cur.day != nxt.day && nxt - cur > 30.minutes) 
+              cur = istencil.index(i  )[:datetime] + interval
+              nxt = istencil.index(i+1)[:datetime] + interval
+              break if (cur.day != nxt.day && (nxt - cur) * 1.day > 30.minutes) 
               break if (cur.hour <= 16 and nxt.hour >= 17)
               i += 1
             end 
-            @next_eod = @istencil.index(i)[:datetime] + @interval
+            @next_eod = istencil.index(i)[:datetime] + interval
           else
             @next_eod
           end
         end
+
+        def next_eow
+          stencil = readcache.cache['stencil'][:obj]
+          now = timezone.now
+          unless @next_eow
+            i = 0
+            i+= 1 while stencil.index(i+1)[:datetime] < now.next_week.beginning_of_week
+            @next_eow = stencil.index(i)[:datetime]   + next_eod.hour.hours + next_eod.min.minutes
+          else
+            @next_eow
+          end
+        end
+        private
+        attr_reader :timezone, :datetime, :readcache, :asset, :interval, :istencil
+
 
       end
     end
