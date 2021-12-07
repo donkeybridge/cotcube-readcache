a181b85d87af876a5a426c7b5e2ced3e186e8ed7
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-12-07 08:23:45 +0000

    models/keys: extending payload with currently supported classes

--- lib/models/keys.rb
+++ lib/models/keys.rb
@@ -11,16 +11,18 @@ module Cotcube
           @timezone  = timezone
           @datetime  = timezone.now
           @keys      = readcache.cache.keys
+          @classes   = Cotcube::ReadCache::Helpers.constants.select {|c| Cotcube::ReadCache::Helpers.const_get(c).is_a? Class}
         end
 
         def update
           @keys      = @readcache.cache.keys
+          @classes   = Cotcube::ReadCache::Helpers.constants.select {|c| Cotcube::ReadCache::Helpers.const_get(c).is_a? Class}
           @datetime  = @timezone.now
           @until     = @datetime + 20.seconds
         end
 
         def payload
-          @keys
+          { keys: @keys, classes: @classes, missing: Cotcube::ReadCache::VALID_ENTITIES.keys.map{|z| z.to_s.upcase.to_sym } - @classes }
         end
 
         def valid_until
